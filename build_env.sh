7a5963623fcc0a165a919dee1daf1bd54334e921
Sergey Tushev <sergy5@mail.ru>
2025-06-19 23:50:30 +0300

    change build env: now works only on Linux

--- build_env.sh
+++ build_env.sh
@@ -2,8 +2,17 @@
 
 set -e
 
-[[ -d .venv ]] || python -m venv .venv
-source .venv/Scripts/activate
-python -m pip install --upgrade pip
+# Проверка, что ОС не Windows
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" || "$OSTYPE" == "win32" ]]; then
+    echo "Error: This script can only be run on Linux systems"
+    exit 1
+fi
+
+# Создание виртуального окружения, если его нет
+[[ -d .venv ]] || python3 -m venv .venv
+
+# Активация и установка зависимостей
+source .venv/bin/activate
+python3 -m pip install --upgrade pip
 pip install uv
 uv pip install -r requirements.txt
